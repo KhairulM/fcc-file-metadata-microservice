46320c4e561e39f1c68438040f35eda3e316fde3
Khairul Makirin <khairul.makirin@caditglobal.com>
2023-04-06 09:51:42 +0700

    finish challenge

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+var multer = require('multer');
+var upload = multer({ dest: './public/data/uploads/' });
+
 var app = express();
 
 app.use(cors());
@@ -11,7 +14,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    'name': req.file?.originalname || null,
+    'type': req.file?.mimetype || null,
+    'size': req.file?.size || null
+  });
+});
 
 
 const port = process.env.PORT || 3000;
